6f0b481bd403613222b7f8348327d5f5537a89c9
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-16 13:05:09 +0000

    Complete foundations: 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inFahreinheit) {
+  let res = Number((5/9 * (inFahreinheit - 32)).toFixed(1));
+  if (!Number((res + "").at(-1))) return Math.round(res);
+  else return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inCelsius) {
+  let res = Number(((9/5 * inCelsius) + 32).toFixed(1));
+  if (!Number((res + "").at(-1))) return Math.round(res);
+  else return res;
 };
 
 // Do not edit below this line
