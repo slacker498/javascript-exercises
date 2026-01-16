027737b86ade4ab74a2cd3e9245260dd86a3b8ae
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-16 11:08:35 +0000

    Complete foundations: 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = "";
+    for (let i = word.length - 1; i >= 0; i--) reverse += word[i];
+    return reverse; 
 };
 
 // Do not edit below this line
