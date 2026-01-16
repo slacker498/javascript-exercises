b506fa82145897d44f245b6848913a0b2ab318a2
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-16 11:03:42 +0000

    Complete foundations: 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num<0) return "ERROR";
+    word = "";
+    for (let i = 0; i < num; i++) word += str;
+    return word;
 };
 
 // Do not edit below this line
