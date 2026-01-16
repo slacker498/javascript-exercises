339b4bd9c91c31bf9f1073d7ac2aa929edd6baa5
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-16 12:11:36 +0000

    Complete foundations: 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a<0 || typeof a !== "number" || a/Math.floor(a) != 1) ||
+        (b<0 || typeof b !== "number" || b/Math.floor(b) != 1)
+    ) return "ERROR";
+    if (b < a) {
+        let temp = b;
+        b = a;
+        a = temp;
+    }
 
+    let total = 0;
+    for (let i = a; i <= b; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
