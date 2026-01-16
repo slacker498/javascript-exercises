6f3df838d6a359a76a575efb47601225dbd39308
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-16 11:44:34 +0000

    Complete foundations: 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    let newArr = arr.slice(0, arr.length);
+    for (let item of itemsToRemove) {
+        while (newArr.includes(item)) {
+            newArr.splice(newArr.indexOf(item), 1); 
+        }
+    }     
+    return newArr;  
 };
 
 // Do not edit below this line
