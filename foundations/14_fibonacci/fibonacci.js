e4738147fd8fcc8f79ec526dccac142d0945cd66
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-18 22:40:05 +0000

    Complete foundations: 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    else if (n === 1 || n === 2) return 1;
+    else return fibonacci(n-2) + fibonacci(n-1);
 
 };
 
