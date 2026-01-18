77bb55f7361c063882a618175684f19138f049a5
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-18 22:27:51 +0000

    Complete foundations: 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    for (let punc of ['.', ',', '?', '\n', '!', ' ']) {
+        word = word.replaceAll(punc, ''); // NB:// this approach does not consider potential cases
+    }
+    return word.toLowerCase() === (Array.from(word)).reverse().join('').toLowerCase();
 };
 
 // Do not edit below this line
