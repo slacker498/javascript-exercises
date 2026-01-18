eee1905c52688f5d5710a4ff01ab22ab04ddcf5c
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-18 21:57:45 +0000

    Complete foundations: 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1) + Number(num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Number(num1) - Number(num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0) return 0;
+	return arr.reduce( (total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (pdt, num) => pdt * num, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let res = 1;
+  for (let i = num; i > 1; i--) {
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
