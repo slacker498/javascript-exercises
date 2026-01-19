51e3cf88b7ef78895cd5b938a1d1f5c4812cbfe2
Jachin Kpogli <jachinkpogli@gmail.com>
2026-01-19 08:49:34 +0000

    Complete foundations: 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    // Initial solution
+    // let oldestPerson = arr[0];
+    // let oldestAge = (oldestPerson.yearOfDeath || (new Date()).getFullYear()) - oldestPerson.yearOfBirth;
+    // let cnt = 0;
+    // do {
+    //     let currPerson = arr[cnt];
+    //     let currPersonAge = (currPerson.yearOfDeath || (new Date()).getFullYear()) - currPerson.yearOfBirth;
+    //     if (currPersonAge > oldestAge) {
+    //         oldestPerson = currPerson;
+    //         oldestAge = currPersonAge;
+    //     }
+    //     cnt++;
+    // } while (cnt < arr.length);
+    // return oldestPerson;
+    
+    return arr.reduce((oldestPerson, currPerson) => {
+        let currPersonAge = (currPerson.yearOfDeath || (new Date()).getFullYear()) - currPerson.yearOfBirth;
+        let oldestAge = (oldestPerson["yearOfDeath"] || (new Date()).getFullYear()) - oldestPerson.yearOfBirth;
+        return (currPersonAge > oldestAge) ? currPerson: oldestPerson;   
+    });
 };
 
 // Do not edit below this line
