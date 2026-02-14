dc42de6df749933f41bc3949ccdef8c33bed2de5
Jachin Kpogli <jachinkpogli@gmail.com>
2026-02-14 14:32:55 +0000

    Complete computer_science: recursion 02

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,17 @@
-const contains = function() {
-  
+const contains = function(obj, val) {
+    // Base steps
+    if (typeof obj !== "object" || Array.isArray(obj)) return Object.is(obj, val)
+    else if (!obj) return Object.is(obj, val);
+
+    // Recursive step
+    else {
+        let isPresent = false;
+        for (let o of Object.values(obj)) {
+            isPresent ||= contains(o, val);   
+            if (isPresent) break;
+        }
+        return isPresent;
+    }
 };
   
 // Do not edit below this line
