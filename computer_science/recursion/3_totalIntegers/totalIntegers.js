89221498dd77a8e0df3b07981813e68af71fe0f0
Jachin Kpogli <jachinkpogli@gmail.com>
2026-02-14 20:59:48 +0000

    Complete 90% of computer_science: recursion 03

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,15 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  if (typeof obj == "number" && obj <= 0 && Number.isFinite(obj) && Number.isInteger(obj)) return 1;
+  if (!obj || typeof obj == "string") return 0;
+  if (typeof obj == "number" && obj / obj.toFixed(0) == 1) return 1;
+  else {
+    let sum = 0;
+    for (let o of Object.values(obj)) {
+      sum += totalIntegers(o);
+    }
+    return sum;
+  }
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
