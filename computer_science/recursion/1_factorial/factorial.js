d7c9412e381bc480e2e3ae389b99ea2ab762c518
Jachin Kpogli <jachinkpogli@gmail.com>
2026-02-14 11:02:52 +0000

    Complete computer_science: recursion 01

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,13 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (
+    !(typeof n === "number") ||
+    n < 0 ||
+    (n != 0 && n / Number(n.toFixed(0)) != 1)
+  )
+    return undefined;
 
+  if (n == 0 || n == 1) return 1;
+  else return n * factorial(n - 1);
 };
 
 // Do not edit below this line
