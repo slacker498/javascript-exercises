c6f571ade5a0662a624d42b0008548ef449ee140
Jachin Kpogli <jachinkpogli@gmail.com>
2026-02-15 15:28:33 +0000

    Complete (with searching for help) recursion: 04

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,25 @@
-const permutations = function() {
-  
+// Code was obtained from gdg. I struggle to understand this :(
+
+const recurPermute = function (index, arr, ans) {
+  if (index == arr.length) {
+    ans.push(arr.slice());
+    return;
+  }
+
+   for (let i = index; i < arr.length; i++) {
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+    recurPermute(index + 1, arr, ans);
+    [arr[index], arr[i]] = [arr[i], arr[index]];
+  }
+}
+
+const permutations = function(array) {
+  let ans = [];
+  recurPermute(0, array, ans);
+  return ans;
 };
+
+console.log(permutations([1, 2]));
   
 // Do not edit below this line
 module.exports = permutations;
