13b4aca1b740ddb5b80e36c0a7fcf373445d03c2
Jachin Kpogli <jachinkpogli@gmail.com>
2026-02-16 00:11:50 +0000

    Failed to do recursion: 05 and just copied

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,20 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    let res = [1];
+
+    if (n == 1) {
+        return res;
+    } 
+    
+    const previousLine = pascal(n - 1);
+    previousLine.forEach((number, i) => {
+        const rightNeighbor = previousLine[i + 1] || 0;
+        res.push(number + rightNeighbor);
+    })
+
+    return res;
 };
+
+console.log(pascal(4))
   
 // Do not edit below this line
 module.exports = pascal;
